42f113fda283ddab3184a96b2d2a08bd73108f67
tatiana talavera <iced.tati@gmail.com>
2023-09-19 18:16:20 -0300

    created assignment form

---
+++ APC-Mini-Remapper/Assignment.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace APC_Mini_Remapper
+{
+    partial class AssignHotkey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            label1 = new Label();
+            keySelectionGroup = new GroupBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            mod1 = new ComboBox();
+            charKey = new ComboBox();
+            mod2 = new ComboBox();
+            tabPage3 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            keySelectionGroup.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.AccessibleName = "Actions";
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(1, 2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(262, 361);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.AccessibleRole = AccessibleRole.None;
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(keySelectionGroup);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(254, 333);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Button Presses";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(254, 333);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "LEDs";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(13, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(227, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Choose a button combination for this key";
+            // 
+            // keySelectionGroup
+            // 
+            keySelectionGroup.Controls.Add(label4);
+            keySelectionGroup.Controls.Add(label3);
+            keySelectionGroup.Controls.Add(label2);
+            keySelectionGroup.Controls.Add(mod1);
+            keySelectionGroup.Controls.Add(charKey);
+            keySelectionGroup.Controls.Add(mod2);
+            keySelectionGroup.Location = new Point(13, 43);
+            keySelectionGroup.Name = "keySelectionGroup";
+            keySelectionGroup.Size = new Size(233, 217);
+            keySelectionGroup.TabIndex = 7;
+            keySelectionGroup.TabStop = false;
+            keySelectionGroup.Text = "Key Selection";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 151);
+            label4.Name = "label4";
+            label4.Size = new Size(26, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Key";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(107, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Modifier (optional)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Modifier (optional)";
+            // 
+            // mod1
+            // 
+            mod1.AllowDrop = true;
+            mod1.DropDownStyle = ComboBoxStyle.DropDownList;
+            mod1.FormattingEnabled = true;
+            mod1.Location = new Point(6, 43);
+            mod1.Name = "mod1";
+            mod1.Size = new Size(221, 23);
+            mod1.TabIndex = 0;
+            // 
+            // charKey
+            // 
+            charKey.DropDownStyle = ComboBoxStyle.DropDownList;
+            charKey.FormattingEnabled = true;
+            charKey.Location = new Point(6, 169);
+            charKey.Name = "charKey";
+            charKey.Size = new Size(221, 23);
+            charKey.TabIndex = 2;
+            // 
+            // mod2
+            // 
+            mod2.DropDownStyle = ComboBoxStyle.DropDownList;
+            mod2.FormattingEnabled = true;
+            mod2.Location = new Point(6, 105);
+            mod2.Name = "mod2";
+            mod2.Size = new Size(221, 23);
+            mod2.TabIndex = 3;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(254, 333);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Events";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // AssignHotkey
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(263, 296);
+            Controls.Add(tabControl1);
+            Name = "AssignHotkey";
+            Text = "Button Settings";
+            Load += Form2_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            keySelectionGroup.ResumeLayout(false);
+            keySelectionGroup.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label1;
+        private GroupBox keySelectionGroup;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private ComboBox mod1;
+        private ComboBox charKey;
+        private ComboBox mod2;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+    }
+}
---
+++ APC-Mini-Remapper/Assignment.cs
@@ -0,0 +1,127 @@
+﻿using WindowsInput;
+using VKC = WindowsInput.Native.VirtualKeyCode;
+
+namespace APC_Mini_Remapper
+{
+    public partial class AssignHotkey : Form
+    {
+        List<KeyId> charKeys = new List<KeyId>() {
+            new KeyId("NONE", VKC.SPACE),
+            new KeyId("SPACE", VKC.SPACE),
+            new KeyId("A", VKC.VK_A),
+            new KeyId("B", VKC.VK_B),
+            new KeyId("C", VKC.VK_C),
+            new KeyId("D", VKC.VK_D),
+            new KeyId("E", VKC.VK_E),
+            new KeyId("F", VKC.VK_F),
+            new KeyId("G", VKC.VK_G),
+            new KeyId("H", VKC.VK_H),
+            new KeyId("I", VKC.VK_I),
+            new KeyId("J", VKC.VK_J),
+            new KeyId("K", VKC.VK_K),
+            new KeyId("L", VKC.VK_L),
+            new KeyId("M", VKC.VK_M),
+            new KeyId("N", VKC.VK_N),
+            new KeyId("O", VKC.VK_O),
+            new KeyId("P", VKC.VK_P),
+            new KeyId("Q", VKC.VK_Q),
+            new KeyId("R", VKC.VK_R),
+            new KeyId("S", VKC.VK_S),
+            new KeyId("T", VKC.VK_T),
+            new KeyId("U", VKC.VK_U),
+            new KeyId("V", VKC.VK_V),
+            new KeyId("W", VKC.VK_W),
+            new KeyId("X", VKC.VK_X),
+            new KeyId("Y", VKC.VK_Y),
+            new KeyId("Z", VKC.VK_Z),
+            new KeyId("0", VKC.VK_0),
+            new KeyId("1", VKC.VK_1),
+            new KeyId("2", VKC.VK_2),
+            new KeyId("3", VKC.VK_3),
+            new KeyId("4", VKC.VK_4),
+            new KeyId("5", VKC.VK_5),
+            new KeyId("6", VKC.VK_6),
+            new KeyId("7", VKC.VK_7),
+            new KeyId("8", VKC.VK_8),
+            new KeyId("9", VKC.VK_9),
+            new KeyId("UP", VKC.UP),
+            new KeyId("DOWN", VKC.DOWN),
+            new KeyId("RIGHT", VKC.RIGHT),
+            new KeyId("LEFT", VKC.LEFT),
+            new KeyId("MUTE", VKC.VOLUME_MUTE),
+            new KeyId("VOLUME DOWN", VKC.VOLUME_DOWN),
+            new KeyId("VOLUME UP", VKC.VOLUME_UP),
+            new KeyId("PLAY/PAUSE", VKC.MEDIA_PLAY_PAUSE),
+            new KeyId("STOP", VKC.MEDIA_STOP),
+            new KeyId("NEXT", VKC.MEDIA_NEXT_TRACK),
+            new KeyId("PREV", VKC.MEDIA_PREV_TRACK),
+            new KeyId("TAB", VKC.TAB)
+        };
+
+        List<KeyId> modKeys = new List<KeyId>(){
+            new KeyId("NONE", VKC.SPACE),
+            new KeyId("CTRL", VKC.CONTROL),
+            //new KeyId("LCTRL", VKC.LCONTROL),
+            //new KeyId("RCTRL", VKC.RCONTROL),
+            //new KeyId("LWIN", VKC.LWIN),
+            new KeyId("WIN", VKC.RWIN),
+            new KeyId("ALT", VKC.MENU),
+            new KeyId("SHIFT", VKC.SHIFT),
+        };
+        public AssignHotkey(string name)
+        {
+            InitializeComponent();
+            this.Text = name;
+            foreach (var key in modKeys)
+            {
+                mod1.Items.Add(key.label);
+                mod2.Items.Add(key.label);
+            }
+
+            foreach (var key in charKeys)
+            {
+                charKey.Items.Add(key.label);
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
