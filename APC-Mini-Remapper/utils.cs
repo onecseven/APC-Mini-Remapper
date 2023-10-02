050c6a16bbaf42ea2cf66bcf1e3ca963d085f051
tatiana talavera <iced.tati@gmail.com>
2023-10-02 18:12:41 -0300

    minor / dont remember

--- APC-Mini-Remapper/utils.cs
+++ APC-Mini-Remapper/utils.cs
@@ -1,13 +1,1 @@
-﻿using WindowsInput.Native;
-
-class KeyId
-{
-    public readonly string label;
-    public readonly VirtualKeyCode value;
-    public KeyId(string label, VirtualKeyCode value)
-    {
-        this.label = label;
-        this.value = value;
-    }
-}
-
+﻿
